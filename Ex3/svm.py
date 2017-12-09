d27f4d48854c6e72a7399ad3252742ba60ba7bf2
orperel <orr.perel@gmail.com>
2017-12-09 15:44:39 +0200

    knn

---
+++ Ex3/svm.py
@@ -0,0 +1,76 @@
+import numpy as np
+import hw3
+import argparse
+from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+
+
+class SVM:
+    def __init__(self, c):
+        self.model = LinearSVC(C=c, loss='hinge', fit_intercept=False)
+
+    def train(self, x, y):
+        self.model.fit(X=x, y=y)
+
+    def predict(self, x):
+        return self.model.predict(X=x)
+
+
+def calculate_accuracy(predictions, labels):
+    ''' Calculate the accuracy for the SVM algorithm on the test set against a subset of the training set'''
+    processed_results = np.subtract(predictions, labels)
+    correct_results = np.count_nonzero(processed_results == 0)
+
+    return float(correct_results) / len(predictions) * 100
+
+
+def plot(x, y1, y2, y1legend, y2legend,
+         x_min, x_max, y_min, y_max,
+         xlabel, ylabel, title):
+    '''
+    Plots the X & Y samples on the canvas, using min - max range for the axes, X is a logarithmic axis
+    '''
+    plt.plot(x, y1, color='red', label=y1legend)
+    plt.plot(x, y2, color='blue', label=y2legend)
+    axes = plt.gca()
+    axes.set_xlim([x_min, x_max])
+    axes.set_ylim([y_min, y_max])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+
+    plt.xscale('log')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(title + ".png")
+    plt.show()
+
+
+def ex_2_a():
+    C_range = [10**x for x in range(-10, 11, 1)]
+
+    training_accuracies = []
+    validation_accuracies = []
+
+    for c in C_range:
+        svm = SVM(c)
+        svm.train(x=hw3.train_data, y=hw3.train_labels)
+        training_predictions = svm.predict(x=hw3.train_data)
+        validation_predictions = svm.predict(x=hw3.validation_data)
+        training_accuracy = calculate_accuracy(training_predictions, hw3.train_labels)
+        validation_accuracy = calculate_accuracy(validation_predictions, hw3.validation_labels)
+        training_accuracies.append(training_accuracy)
+        validation_accuracies.append(validation_accuracy)
+
+    y_min = min(min(training_accuracies), min(validation_accuracies)) - 1
+    y_max = max(max(training_accuracies), max(validation_accuracies)) + 1
+    plot(x=C_range, y1=training_accuracies, y1legend='Training Accuracies', y2=validation_accuracies, y2legend='Validation Accuracies',
+         x_min=C_range[0], x_max=C_range[-1], y_min=y_min, y_max=y_max,
+         xlabel='C [SVM penalty Regularization]', ylabel='Accuracy', title='Section 3.A - SVM Accuracy for C')
+
+
+parser = argparse.ArgumentParser(description='Choose a subsection')
+parser.add_argument('--section', help='A section of the ex')
+args = parser.parse_args()
+if args.section == '1':
+    ex_2_a()
